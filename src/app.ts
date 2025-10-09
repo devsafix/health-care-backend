503411141cd02f16b45111961a905ee114c844b6
devsafix <devsafix@gmail.com>
2025-10-09 10:40:31 +0600

    Add: User and Enum Schema

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,29 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import notFound from './app/middlewares/notFound';
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import config from "./config";
 
 const app: Application = express();
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send({
-        Message: "Ph health care server.."
-    })
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "Health care server is running...",
+    environment: config.node_env,
+    uptime: process.uptime().toFixed() + " Seconds",
+    timeStamp: new Date().toLocaleDateString(),
+  });
 });
-
 
 app.use(globalErrorHandler);
 
