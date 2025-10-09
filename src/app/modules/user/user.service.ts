383338815913d55acfa516f89e5134a38008b4c6
devsafix <devsafix@gmail.com>
2025-10-09 15:55:20 +0600

    Create: Patient create service

---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Request } from "express";
+import bcrypt from "bcryptjs";
+import { prisma } from "../../shared/prisma";
+import { createPatientInput } from "./user.interface";
+
+const createPatient = async (payload: createPatientInput) => {
+  const hashPassword = await bcrypt.hash(payload.password, 10);
+
+  const result = await prisma.$transaction(async (tnx) => {
+    await tnx.user.create({
+      data: {
+        email: payload.email,
+        password: hashPassword,
+      },
+    });
+
+    return await tnx.patient.create({
+      data: {
+        name: payload.name,
+        email: payload.email,
+      },
+    });
+  });
+
+  return result;
+};
+
+export const UserService = {
+  createPatient,
+};
