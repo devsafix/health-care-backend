9cd5a720e51cf265a4d2ff545904fa2496eb1b1a
devsafix <devsafix@gmail.com>
2025-10-10 08:57:25 +0600

    Config: Cloudinary for register api

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,24 +1,26 @@
 import { Request } from "express";
 import bcrypt from "bcryptjs";
 import { prisma } from "../../shared/prisma";
-import { createPatientInput } from "./user.interface";
+import { fileUploader } from "../../helper/fileUploader";
 
-const createPatient = async (payload: createPatientInput) => {
-  const hashPassword = await bcrypt.hash(payload.password, 10);
+const createPatient = async (req: Request) => {
+  if (req.file) {
+    const uploadResult = await fileUploader.uploadToCloudinary(req.file);
+    req.body.patient.profilePhoto = uploadResult?.secure_url;
+  }
+
+  const hashPassword = await bcrypt.hash(req.body.password, 10);
 
   const result = await prisma.$transaction(async (tnx) => {
     await tnx.user.create({
       data: {
-        email: payload.email,
+        email: req.body.patient.email,
         password: hashPassword,
       },
     });
 
     return await tnx.patient.create({
-      data: {
-        name: payload.name,
-        email: payload.email,
-      },
+      data: req.body.patient,
     });
   });
 
