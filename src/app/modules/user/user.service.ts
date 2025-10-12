1b1010c4e0660220ab9f1b7bd707c6430e842be8
devsafix <devsafix@gmail.com>
2025-10-12 07:40:53 +0600

    Create: Service for create admin

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,6 +2,7 @@ import { Request } from "express";
 import bcrypt from "bcryptjs";
 import { prisma } from "../../shared/prisma";
 import { fileUploader } from "../../helper/fileUploader";
+import { Admin, UserRole } from "@prisma/client";
 
 const createPatient = async (req: Request) => {
   if (req.file) {
@@ -27,6 +28,38 @@ const createPatient = async (req: Request) => {
   return result;
 };
 
+const createAdmin = async (req: Request): Promise<Admin> => {
+  const file = req.file;
+
+  if (file) {
+    const uploadToCloudinary = await fileUploader.uploadToCloudinary(file);
+    req.body.admin.profilePhoto = uploadToCloudinary?.secure_url;
+  }
+
+  const hashedPassword: string = await bcrypt.hash(req.body.password, 10);
+
+  const userData = {
+    email: req.body.admin.email,
+    password: hashedPassword,
+    role: UserRole.ADMIN,
+  };
+
+  const result = await prisma.$transaction(async (transactionClient) => {
+    await transactionClient.user.create({
+      data: userData,
+    });
+
+    const createdAdminData = await transactionClient.admin.create({
+      data: req.body.admin,
+    });
+
+    return createdAdminData;
+  });
+
+  return result;
+};
+
 export const UserService = {
   createPatient,
+  createAdmin,
 };
