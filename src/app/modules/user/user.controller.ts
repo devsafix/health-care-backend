46e4f58489f2e7f14c639faf191c1bd10e201b4c
devsafix <devsafix@gmail.com>
2025-10-12 07:43:25 +0600

    Create: Create Doctor Controller

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -24,7 +24,18 @@ const createAdmin = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const createDoctor = catchAsync(async (req: Request, res: Response) => {
+  const result = await UserService.createDoctor(req);
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: "Doctor Created successfuly!",
+    data: result,
+  });
+});
+
 export const UserController = {
   createPatient,
   createAdmin,
+  createDoctor,
 };
