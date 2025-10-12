9464f3753b639b518edb792a33ec728928dfb6c7
devsafix <devsafix@gmail.com>
2025-10-12 07:42:47 +0600

    Add: Create Doctor route

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -30,4 +30,17 @@ router.post(
   }
 );
 
+router.post(
+  "/create-doctor",
+  auth(UserRole.ADMIN),
+  fileUploader.upload.single("file"),
+  (req: Request, res: Response, next: NextFunction) => {
+    console.log(JSON.parse(req.body.data));
+    req.body = UserValidation.createDoctorValidationSchema.parse(
+      JSON.parse(req.body.data)
+    );
+    return UserController.createDoctor(req, res, next);
+  }
+);
+
 export const userRoutes = router;
