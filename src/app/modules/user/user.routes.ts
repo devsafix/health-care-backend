b3db0c2d4a779b94eb8cab74873e87a5f0c916a4
devsafix <devsafix@gmail.com>
2025-10-12 07:39:03 +0600

    Create: Auth middleware and add create admin route

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -2,6 +2,8 @@ import express, { NextFunction, Request, Response } from "express";
 import { UserController } from "./user.controller";
 import { fileUploader } from "../../helper/fileUploader";
 import { UserValidation } from "./user.validation";
+import { UserRole } from "@prisma/client";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
@@ -13,6 +15,18 @@ router.post(
       JSON.parse(req.body.data)
     );
     return UserController.createPatient(req, res, next);
+  }
+);
+
+router.post(
+  "/create-admin",
+  auth(UserRole.ADMIN),
+  fileUploader.upload.single("file"),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = UserValidation.createAdminValidationSchema.parse(
+      JSON.parse(req.body.data)
+    );
+    return UserController.createAdmin(req, res, next);
   }
 );
 
