38aeaab5063bf861ec2c25e42658a78c14bf04d0
devsafix <devsafix@gmail.com>
2025-10-13 11:10:48 +0600

    Create: Schedule Create Service

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { userRoutes } from "../modules/user/user.routes";
 import { authRoutes } from "../modules/auth/auth.routes";
+import { ScheduleRoutes } from "../modules/schedule/schedule.routes";
 
 const router = express.Router();
 
@@ -12,6 +13,10 @@ const moduleRoutes = [
   {
     path: "/auth",
     route: authRoutes,
+  },
+  {
+    path: "/schedule",
+    route: ScheduleRoutes,
   },
 ];
 
