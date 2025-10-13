0ca90bf710403736096896ca76031d80747c6a1d
devsafix <devsafix@gmail.com>
2025-10-13 11:14:24 +0600

    Create: Controller for schedules For Doctor

--- src/app/modules/schedule/schedule.controller.ts
+++ src/app/modules/schedule/schedule.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import { ScheduleService } from "./schedule.service";
+import { JwtPayload } from "jsonwebtoken";
+import pick from "../../helper/pick";
 
 const createSchedule = catchAsync(async (req: Request, res: Response) => {
   const result = await ScheduleService.createSchedule(req.body);
@@ -14,6 +16,29 @@ const createSchedule = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const schedulesForDoctor = catchAsync(
+  async (req: Request & { user?: JwtPayload }, res: Response) => {
+    const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
+    const filters = pick(req.query, ["startDateTime", "endDateTime"]);
+
+    const user = req.user;
+    const result = await ScheduleService.schedulesForDoctor(
+      user as JwtPayload,
+      filters,
+      options
+    );
+
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Schedule fetched successfully!",
+      meta: result.meta,
+      data: result.data,
+    });
+  }
+);
+
 export const ScheduleController = {
   createSchedule,
+  schedulesForDoctor,
 };
--- src/app/modules/schedule/schedule.routes.ts
+++ src/app/modules/schedule/schedule.routes.ts
@@ -3,6 +3,8 @@ import { ScheduleController } from "./schedule.controller";
 
 const router = express.Router();
 
+router.get("/", ScheduleController.schedulesForDoctor);
+
 router.post("/", ScheduleController.createSchedule);
 
 export const ScheduleRoutes = router;
