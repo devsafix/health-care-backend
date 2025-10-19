b913dcb1c614bbfc8162bdccbfba9915caebfe7c
devsafix <devsafix@gmail.com>
2025-10-19 09:01:48 +0600

    Create: Doctor routes

--- src/app/modules/specialties/specialties.routes.ts
+++ src/app/modules/specialties/specialties.routes.ts
@@ -7,13 +7,6 @@ import { SpecialtiesValidation } from "./specialties.validation";
 
 const router = express.Router();
 
-// Task 1: Retrieve Specialties Data
-
-/**
-- Develop an API endpoint to retrieve all specialties data.
-- Implement an HTTP GET endpoint returning specialties in JSON format.
-- ENDPOINT: /specialties
-*/
 router.get("/", SpecialtiesController.getAllFromDB);
 
 router.post(
@@ -24,15 +17,6 @@ router.post(
     return SpecialtiesController.insertIntoDB(req, res, next);
   }
 );
-
-// Task 2: Delete Specialties Data by ID
-
-/**
-- Develop an API endpoint to delete specialties by ID.
-- Implement an HTTP DELETE endpoint accepting the specialty ID.
-- Delete the specialty from the database and return a success message.
-- ENDPOINT: /specialties/:id
-*/
 
 router.delete(
   "/:id",
