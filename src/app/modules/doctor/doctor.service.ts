c464880df81b6349aa0efedd89aeb536fcbf25e6
devsafix <devsafix@gmail.com>
2025-10-19 09:23:39 +0600

    Create: Doctor Update Service

--- src/app/modules/doctor/doctor.service.ts
+++ src/app/modules/doctor/doctor.service.ts
@@ -80,8 +80,66 @@ const getAllFromDB = async (filters: any, options: IOptions) => {
   };
 };
 
+const updateIntoDB = async (
+  id: string,
+  payload: Partial<IDoctorUpdateInput>
+) => {
+  const doctorInfo = await prisma.doctor.findUniqueOrThrow({
+    where: {
+      id,
+    },
+  });
 
+  const { specialties, ...doctorData } = payload;
+
+  return await prisma.$transaction(async (tnx) => {
+    if (specialties && specialties.length > 0) {
+      const deleteSpecialtyIds = specialties.filter(
+        (specialty) => specialty.isDeleted
+      );
+
+      for (const specialty of deleteSpecialtyIds) {
+        await tnx.doctorSpecialties.deleteMany({
+          where: {
+            doctorId: id,
+            specialtiesId: specialty.specialtyId,
+          },
+        });
+      }
+
+      const createSpecialtyIds = specialties.filter(
+        (specialty) => !specialty.isDeleted
+      );
+
+      for (const specialty of createSpecialtyIds) {
+        await tnx.doctorSpecialties.create({
+          data: {
+            doctorId: id,
+            specialtiesId: specialty.specialtyId,
+          },
+        });
+      }
+    }
+
+    const updatedData = await tnx.doctor.update({
+      where: {
+        id: doctorInfo.id,
+      },
+      data: doctorData,
+      include: {
+        doctorSpecialties: {
+          include: {
+            specialties: true,
+          },
+        },
+      },
+    });
+
+    return updatedData;
+  });
+};
 
 export const DoctorService = {
   getAllFromDB,
+  updateIntoDB,
 };
