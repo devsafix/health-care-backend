a5c6777abf1f6d4e31e03c6b47516d2155fd1dfe
devsafix <devsafix@gmail.com>
2025-10-19 21:13:43 +0600

    Add: Doctor Ai suggestion controller and route

--- src/app/modules/doctor/doctor.routes.ts
+++ src/app/modules/doctor/doctor.routes.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import { DoctorController } from "./doctor.controller";
+import { UserRole } from "@prisma/client";
+import auth from "../../middlewares/auth";
 const router = express.Router();
 
 router.get("/", DoctorController.getAllFromDB);
 
-router.patch("/:id", DoctorController.updateIntoDB);
+router.post("/suggestion", DoctorController.getAISuggestions);
+
+router.patch(
+  "/:id",
+  auth(UserRole.ADMIN, UserRole.DOCTOR),
+  DoctorController.updateIntoDB
+);
 export const DoctorRoutes = router;
