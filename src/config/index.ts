97cce13e88d0f5d8c447d62b5c4fd9775df03af3
devsafix <devsafix@gmail.com>
2025-10-19 21:10:43 +0600

    Config: Open Router API

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,4 +16,5 @@ export default {
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
   },
+  open_router_api_key: process.env.OPEN_ROUTER_API_KEY,
 };
