a689c65b0402e79b0f644137e5d79184cc0c32ed
devsafix <devsafix@gmail.com>
2025-10-21 23:22:22 +0600

    Create: Prescription Creating Controller

--- src/app/modules/prescription/prescription.routes.ts
+++ src/app/modules/prescription/prescription.routes.ts
@@ -2,6 +2,7 @@ import { UserRole } from "@prisma/client";
 import express from "express";
 import auth from "../../middlewares/auth";
 import { PrescriptionController } from "./prescription.controller";
+
 const router = express.Router();
 
 router.post(
