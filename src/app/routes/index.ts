8c8e7ef6d1457076d8042225fd0b4c72232be1f8
devsafix <devsafix@gmail.com>
2025-10-22 10:11:27 +0600

    Add: Patient Routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -8,6 +8,7 @@ import { DoctorRoutes } from "../modules/doctor/doctor.routes";
 import { AppointmentRoutes } from "../modules/appointment/appointment.routes";
 import { PrescriptionRoutes } from "../modules/prescription/prescription.routes";
 import { ReviewRoutes } from "../modules/review/review.routes";
+import { PatientRoutes } from "../modules/patient/patient.routes";
 
 const router = express.Router();
 
@@ -35,6 +36,10 @@ const moduleRoutes = [
   {
     path: "/doctor",
     route: DoctorRoutes,
+  },
+  {
+    path: "/patient",
+    route: PatientRoutes,
   },
   {
     path: "/appointment",
